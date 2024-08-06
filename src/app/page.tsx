d4a69330dbeee54d7a8427b5af8c57e74992237f
João Victor Brandão de Abreu <joao.brandao@clickip.local>
2024-08-06 11:27:39 -0400

    feat: Add HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { Home as HomeIcon, SearchIcon, Library, ChevronLeft, ChevronRight } from "lucide-react";
+import { Home as HomeIcon, SearchIcon, Library, ChevronLeft, ChevronRight, Play, Shuffle, SkipBack, SkipForward, Repeat } from "lucide-react";
 
 export default function Home() {
   return (
@@ -41,7 +41,7 @@ export default function Home() {
         <main className="flex-1 p-6">
           <div className="flex item-center gap-4">
             <button className="w-6 h-6 rounded-full bg-black/40" >
-              <ChevronLeft/>
+              <ChevronLeft />
             </button>
             <button className="w-6 h-6 rounded-full bg-black/40">
               <ChevronRight />
@@ -49,31 +49,114 @@ export default function Home() {
           </div>
 
           <h1 className="font-semibold text-3xl mt-10 ">Good Afternoon</h1>
-          
-          <div className="grid grid-cols-3 gap-6">
-            <div className="bg-white/10">
-              Playlist
-            </div>
-            <div className="bg-white/10">
-              Playlist
-            </div>
-            <div className="bg-white/10">
-              Playlist
-            </div>
-            <div className="bg-white/10">
-              Playlist
-            </div>
-            <div className="bg-white/10">
-              Playlist
-            </div>
-            <div className="bg-white/10">
-              Playlist
-            </div>
+
+          <div className="grid grid-cols-3 gap-4 mt-4 ">
+            <a className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
+              <Image src="/album.jpg" width={104} height={104} alt="Capa do album"></Image>
+              <strong>Circles</strong>
+              <button className=" w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+            <a className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden  hover:bg-white/20 transition-colors">
+              <Image src="/album.jpg" width={104} height={104} alt="Capa do album"></Image>
+              <strong>Circles</strong>
+              <button className=" w-12 h-12 flex items-center justify-center	pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+            <a className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden  hover:bg-white/20 transition-colors">
+              <Image src="/album.jpg" width={104} height={104} alt="Capa do album"></Image>
+              <strong>Circles</strong>
+              <button className=" w-12 h-12 flex items-center justify-center	pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+            <a className="bg-white/5 group rounded flex items-center  gap-4 overflow-hidden  hover:bg-white/20 transition-colors">
+              <Image src="/album.jpg" width={104} height={104} alt="Capa do album"></Image>
+              <strong>Circles</strong>
+              <button className=" w-12 h-12 flex items-center justify-center	pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+            <a className="bg-white/5 group rounded flex items-center  gap-4 overflow-hidden  hover:bg-white/20 transition-colors">
+              <Image src="/album.jpg" width={104} height={104} alt="Capa do album"></Image>
+              <strong>Circles</strong>
+              <button className=" w-12 h-12 flex items-center justify-center	pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
+            <a className="bg-white/5 group rounded flex items-center  gap-4 overflow-hidden  hover:bg-white/20 transition-colors">
+              <Image src="/album.jpg" width={104} height={104} alt="Capa do album"></Image>
+              <strong>Circles</strong>
+              <button className=" w-12 h-12 flex items-center justify-center	pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+                <Play />
+              </button>
+            </a>
           </div>
 
+          <h2 className="font-semibold text-3xl mt-10 ">Made For You</h2>
+
+          <div className="grid grid-cols-6 mt-4 gap-4">
+            < a href="" className="bg-white/5 p-2 rounded-lg flex flex-col hover:bg-white/10">
+              <Image src="/album.jpg" className="w-full" width={104} height={104} alt="Capa do album"></Image>
+              <strong className="font-semibold">Daily Mix 1</strong>
+              <span className="text-sm text-zinc-400">Vista Kicks,Circles</span>              
+            </a>           
+            < a href="" className="bg-white/5 p-2 rounded-lg flex flex-col hover:bg-white/10">
+              <Image src="/album.jpg" className="w-full" width={104} height={104} alt="Capa do album"></Image>
+                <strong className="font-semibold">Daily Mix 1</strong>
+                <span className="text-sm text-zinc-400">Vista Kicks,Circles</span>
+            </a>             
+            <a href="" className="bg-white/5 p-2 rounded-lg flex flex-col hover:bg-white/10">
+              <Image src="/album.jpg" className="w-full" width={104} height={104} alt="Capa do album"></Image>
+                <strong className="font-semibold">Daily Mix 1</strong>
+                <span className="text-sm text-zinc-400">Vista Kicks,Circles</span>
+            </a>          
+            <a href="" className="bg-white/5 p-2 rounded-lg flex flex-col hover:bg-white/10">
+              <Image src="/album.jpg" className="w-full" width={104} height={104} alt="Capa do album"></Image>
+                <strong className="font-semibold">Daily Mix 1</strong>
+                <span className="text-sm text-zinc-400">Vista Kicks,Circles</span>
+            </a>          
+            < a href="" className="bg-white/5 p-2 rounded-lg flex flex-col hover:bg-white/10">
+              <Image src="/album.jpg" className="w-full" width={104} height={104} alt="Capa do album"></Image>
+                <strong className="font-semibold">Daily Mix 1</strong>
+                <span className="text-sm text-zinc-400">Vista Kicks,Circles</span>
+            </a> 
+          </div>
         </main>
       </div>
-      <footer className="bg-zinc-800 border-t border-zinc-700 p-6"> footer</footer>
+      <footer className="bg-zinc-800 border-t border-zinc-700 p-6 flex items-center justify-between"> 
+        
+        <div className="flex items-center gap-3">
+          <Image src="/album.jpg" className="w-full" width={56} height={56} alt="Capa do album"></Image>
+          <div className="flex flex-col gap-1">
+            <strong className="font-normal">Marceline</strong>
+            <span className="text-xs text-zinc-400">Vista Kicks</span>
+          </div>
+        </div>
+
+        <div className="flex flex-col items-center">
+          <div className="flex items-center gap-6">
+            <Shuffle size={20} className="text-zinc-200"/>
+            <SkipBack size={20} className="text-zinc-200"/>
+
+            <button className=" w-10 h-10 flex items-center justify-center pl-1 rounded-full bg-white text-black ">
+            <Play ></Play>
+            </button>
+
+            <SkipForward size={20} className="text-zinc-200"/>       
+            <Repeat size={20} className="text-zinc-200"/>
+          </div>
+
+          <div className="flex items-center gap-2">
+            <span className="text-sx"></span>
+          </div>
+        </div>
+        <div>
+
+        </div>
+      </footer>
     </div>
   );
 }
